425d3f99eeadc85fd55b24299b07151f87e20a51
youknowznm <znm_92@163.com>
2016-10-04 17:56:16 +0800

    upd

--- src/script/main.js
+++ src/script/main.js
@@ -5,3 +5,26 @@
 //   el: '#app',
 //   render: h => h(App)
 // })
+
+$(window).on('resize load', function() {
+  //
+  var memoWidth = $('.memo').eq(0).width();
+  $('.memo').height(memoWidth + 50);
+  //
+  var popupTargetHeight = $('.popup').width() + 50;
+  $('.popup')
+    .height(popupTargetHeight)
+    .css('marginTop', -(popupTargetHeight / 2))
+    .css('marginLeft', -(popupTargetHeight / 2));
+  //
+  $('.edit-content').height(popupTargetHeight - 64);
+
+});
+
+$('body').on('mousemove', function(e) {
+  bcgOffsetX = Math.ceil(e.pageX / 30);
+  bcgOffsetY = Math.ceil(e.pageY / 30);
+  $('.memos')
+    .css('background-position-x', bcgOffsetX)
+    .css('background-position-y', bcgOffsetY);
+});
