5a0f193a4d8a9f9e09915cb394a95b32f5c4278b
youknowznm <znm_92@163.com>
2016-10-07 15:46:42 +0800

    updated canvas util

--- src/main.js
+++ src/main.js
@@ -4,51 +4,30 @@
 // new Vue({
 //   el: '#app',
 //   render: h => h(App)
-// })
+//
 
-$(window).on('resize load', function() {
+$(window).on('resize load', () => {
   //
-  var memoWidth = $('.memo').eq(0).width();
-  $('.memo').height(memoWidth + 50);
+  let editLayerHeight = $('.editor-layer').width() + 50;
+  $('#editor-text')
+    .height(editLayerHeight)
+    .css('marginTop', -(editLayerHeight / 2))
+    .css('marginLeft', -(editLayerHeight / 2));
   //
-  var popupTargetHeight = $('.popup').width() + 50;
-  $('.popup')
-    .height(popupTargetHeight)
-    .css('marginTop', -(popupTargetHeight / 2))
-    .css('marginLeft', -(popupTargetHeight / 2));
-  //
-  $('#edit-text-content').height(popupTargetHeight - 64);
-  $('#edit-doodle-content')
-    .attr('width', popupTargetHeight - 50)
-    .attr('height', popupTargetHeight - 50);
+  $('#editor-text-content').height(editLayerHeight - 64);
 
 });
 
-$('body').on('mousemove', function(e) {
-  bcgOffsetX = Math.ceil(e.pageX / 30);
-  bcgOffsetY = Math.ceil(e.pageY / 30);
+$('body').on('mousemove', (e) => {
   $('.memos')
-    .css('background-position-x', bcgOffsetX)
-    .css('background-position-y', bcgOffsetY);
+    .css('background-position-x', Math.ceil(e.pageX / 30))
+    .css('background-position-y', Math.ceil(e.pageY / 30));
 });
 
 
-var getDoodle = function(canvasEle, imageData) {
-  // console.log(canvasEle.offsetLeft, canvas.offsetTop);
-  var canvas = $(canvasEle);
-  var colors = document.createElement('ul');
-  $('colors')
-    .addClass('')
-  // colors.
-  // <ul class="colors">
-  //   <li class="black"
-  //     v-for="color in colors"
-  //     :class="color.name"
-  //     :data-selected="color.name === selectedColor.name ? true : false"
-  //     @click="setColor(color.name)">
-  //   </li>
-  // </ul>`;
-  var colors = [
+const setCanvas = (canvasEle, colorsEle, controllersEle, imageData) => {
+  // 颜色表对象
+  const colorTable = [
     {
       name: 'black',
       regularCode: '#222',
@@ -75,7 +54,105 @@ var getDoodle = function(canvasEle, imageData) {
       opagueCode: '#fff',
     },
   ];
+  // get context
+  let ctx = canvasEle.getContext('2d');
+  let selectedColor = null;
+  let hasOnGoingStroke = false;
+  let statusStack = [];
+  let $canvas = $(canvasEle);
+  // 方法：将 imageData 写入一个 Image 对象，画在 canvas 上
+  const loadImageData = (data) => {
+    let img = new Image();
+    img.src = data;
+    clearCanvas();
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+    };
+  }
+  // 方法：存储当前的 canvas 内容为 imageData，推入状态栈
+  const saveImageData = () => {
+    let currentStatus = canvasEle.toDataURL(0, 0, 300, 300);
+    statusStack.push(currentStatus);
+  };
+  // 方法：清除画布
+  const clearCanvas = () => {
+    ctx.clearRect(0, 0, 300, 300);
+  };
+  // 提供了 imageData 参数，就画出来，
+  if (imageData !== null) {
+    loadImageData(imageData);
+  }
+  // 无论此时是否已有内容，推入状态栈一次
+  saveImageData();
+  // 颜色选择器鼠标事件
+  $(colorsEle)
+    .on('click', (evt) => {
+      $target = $(evt.target);
+      const tarColorName = $target.data('color');
+      selectedColor = colorTable.find( (item) => {
+        return item.name === tarColorName;
+      });
+      $target
+        .siblings('.current')
+        .removeClass('current')
+        .end()
+        .addClass('current');
+    })
+    .children('[data-color=black]')
+    .click();
+  // canvas 鼠标事件
+  $canvas.on('mousedown', (evt) => {
+      hasOnGoingStroke = true;
+      ctx.strokeStyle = selectedColor.opagueCode;
+      ctx.lineWidth = 5;
+      ctx.lineCap = 'round';
+      ctx.lineJoin = 'round';
+      ctx.imageSmoothingEnabled = true;
+      ctx.beginPath();
+      ctx.moveTo(evt.offsetX, evt.offsetY);
+    })
+    .on('mousemove', (evt) => {
+      if (hasOnGoingStroke === true) {
+        ctx.lineTo(evt.offsetX, evt.offsetY);
+        ctx.stroke();
+      }
+    })
+    .on('mouseout mouseup', (evt) => {
+      if (hasOnGoingStroke === true) {
+        ctx.strokeStyle = selectedColor.regularCode;
+        ctx.lineTo(evt.offsetX, evt.offsetY);
+        ctx.stroke();
+        hasOnGoingStroke = false;
+        saveImageData();
+      }
+    });
+
+  // 控制器鼠标事件
+  $(controllersEle)
+    .children('.undo')
+    .on('click', (evt) => {
+      if (!$(evt.target).hasClass('disabled')) {
+        loadImageData(statusStack[statusStack.length - 2]);
+      }
+    })
+    .end()
+    .children('.redo')
+    .on('click', (evt) => {
+      if (!$(evt.target).hasClass('disabled')) {
+        saveImageData();
+      }
+    })
+    .end()
+    .children('.clear')
+    .on('click', () => {
+      clearCanvas();
+    });
+
 
 }
 
-getDoodle(document.getElementById('edit-doodle-content'), null)
+setCanvas(
+  document.getElementById('editor-doodle-content'),
+  $('#editor-doodle .colors'),
+  $('#editor-doodle .controllers'),
+  null);
